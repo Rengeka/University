49685ee19cea45c2a741458ce6c3b34954e4f677
Rengeka <stasik2004x@gmail.com>
2025-02-10 15:36:17 +0200

    PHP Lab 1 was added

---
+++ YearTwo/PHP/Lab1/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Привет, мир!";
+
+echo "Hello, World with echo!";
+print "Hello, World with print!";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+?>
+
+<br><?php
+echo $days . $message;
+?>
+<br><?php
+echo "$days $message";
